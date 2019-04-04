bc06ae66aab8adf57f50d6d48ce8167ea4d18f6d
hulian <736904704@qq.com>
2019-04-04 23:17:39 +0800

    V0.1

---
+++ src/main/java/com/enjoy/App.java
@@ -0,0 +1,26 @@
+package com.enjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class App {
+
+	/**
+	 * 通过main方法，作为启动的入口
+	 * */
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+	@RequestMapping("/hello")
+	public String hello(){
+		return "hello, this is fisrt use git !";
+	}
+	
+}
+
+
