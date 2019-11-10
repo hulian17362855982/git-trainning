ad854c54776765aac37249db85ffc04827ed6ce2
hulian <c-hul01@scpgroup.com>
2019-11-10 23:22:23 +0800

    新增一个说话的方法1

--- src/main/java/cn/enjoy/User.java
+++ src/main/java/cn/enjoy/User.java
@@ -9,10 +9,10 @@ public class User {
     private String classes;
 
     public void print(){
-        System.out.println("user， 您好！！！！！！！！！！！！！！！");
+        System.out.println("user， 您好啊，世界");
     }
 
     public void say(){
-        System.out.println("english !!!!!!!!!!!!!!!!!!!!");
+        System.out.println("chinese !!!!!!!!!!!!!!!@@@");
     }
 }
